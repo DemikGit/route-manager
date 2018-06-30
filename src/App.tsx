70161740bacb204a04414ab9ae7720dfc33b54ad
Alexander Dyumin <AlekGit@yandex.ru>
2018-06-30 18:54:13 +0300

    init application

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  public render() {
+    return null;
+  }
+}
+
+export default App;

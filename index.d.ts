b22d0f57f27a4eb0162de2a26b053b051865b4d4
Alexander Dyumin <AlekGit@yandex.ru>
2018-07-03 14:11:15 +0300

    fix typings for googleMaps

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-google-maps/lib/components/addons/MarkerClusterer' {
+  export interface Calculator { }
+  export interface ClusterIconStyle { }
+  export interface Cluster { }
+}

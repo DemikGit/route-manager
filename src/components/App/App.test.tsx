22237545c23c29fa286705b78d6386baf4f39ca0
Alexander Dyumin <AlekGit@yandex.ru>
2018-07-17 00:50:03 +0300

    add tests for App component

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,7 +1,15 @@
 import { shallow } from 'enzyme';
+import 'jest-enzyme';
 import * as React from 'react';
+import { RouteManager } from '../RouteManager/RouteManager';
 import App from './App';
 
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('have RouteManager component', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toContainReact(<RouteManager />);
+  expect(wrapper).toMatchSnapshot();
 });
